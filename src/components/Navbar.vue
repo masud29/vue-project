1cb5b447e131a6493564b9f13f88a656227e595a
masudrana29 <masudrana06013@gmail.com>
2024-12-09 15:55:06 +0600

    firs vue project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <nav>
+        <ul>
+            <li> <a href="">Home</a> </li>
+            <li> <a href="">Post</a> </li>
+            <li> <a href="">Product</a> </li>
+            <li> <a href="">Contact</a> </li>
+        </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navbar-1',
+  data(){
+    return {
+
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+nav{
+    text-align: center;
+    display: inline-block;
+    list-style: none;
+}
+</style>
